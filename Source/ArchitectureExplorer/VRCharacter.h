a0b7eec2137faca0290ca4041d4b91a9d14b33e1
xuelongmu <xuelongmu@gmail.com>
2020-04-07 16:55:05 -0400

    added locomotion to character

---
+++ Source/ArchitectureExplorer/VRCharacter.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Camera/CameraComponent.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+
+#include "VRCharacter.generated.h"
+
+UCLASS()
+class ARCHITECTUREEXPLORER_API AVRCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AVRCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* Camera;
+
+	void MoveForward(float throttle);
+	void MoveRight(float throttle);
+};
